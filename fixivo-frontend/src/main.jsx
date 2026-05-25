9420d471003d5e8c5734906bcb12640eceab1c0a
yashkhude <khudeyash1708@gmail.com>
2026-05-26 00:49:45 +0530

    google oauth integration

--- fixivo-frontend/src/main.jsx
+++ fixivo-frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 const queryClient = new QueryClient();
@@ -10,7 +10,9 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+        <App />
+      </GoogleOAuthProvider>
     </QueryClientProvider>
-  </Provider>
+  </Provider>,
 );
